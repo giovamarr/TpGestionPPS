86f3564f4cc2418c2e20b080df53323463eb5c25
Giovanni Martin <giovamartin@gmail.com>
2022-11-01 18:24:49 -0300

    punto 16 back

--- app/views/ResponsableHome.php
+++ app/views/ResponsableHome.php
@@ -58,6 +58,19 @@ include 'inc/topScripts.php';
                     </div>
                 </div>
             </div>
+            <div class="card col-md m-2 menu">
+                <div class="card-body">
+                    <h3 class="card-title text-center" alt="Registrar Docentes">PPS Aprobadas</h3>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary " onclick="location.href='ResponsableViewAprovedPPS.php';" alt="ir a Registrar Docentes">
+                            <svg width="20" height="20" fill="currentColor" class="bi bi-arrow-right-circle-fill d-flex align-items-center justify-content-center">
+                                <use xlink:href="#arrow"></use>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
