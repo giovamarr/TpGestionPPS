bdf8509fbff763ea9cd551155ece797c73456f9e
DamianBarzola <dbdamix@gmail.com>
2022-11-04 16:17:42 -0300

    page404

---
+++ app/views/404.php
@@ -0,0 +1,34 @@
+<?php
+include 'inc/topScripts.php';
+?>
+<title>Página no encontrada</title>
+</head>
+<?php
+include 'inc/headerv2.php';
+?>
+<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+    <symbol id="arrow" viewBox="0 0 16 16">
+        <path
+            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z">
+        </path>
+    </symbol>
+</svg>
+
+<div class="container " id="menu">
+    <div class="row row-cols-1 row-cols-lg-3 ">
+        <div class="col-12 d-flex justify-content-center"><img src="https://fbm.es/imagenes/error-404.png" alt="nf">
+        </div>
+        <div class="col-12 d-flex justify-content-center">
+            <h3>Página no encontrada</h3>
+
+        </div>
+        <div class="col-12 d-flex justify-content-center">
+
+            <a href="https://tpgestion-pps.herokuapp.com/index.php">Ir al Inicio</a>
+        </div>
+    </div>
+</div>
+
+<?php
+include 'inc/footerv2.html';
+?>
