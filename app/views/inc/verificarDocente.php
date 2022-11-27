ce5c1df4339c94ac1cb576ad21bb61deea74d31c
DamianBarzola <dbdamix@gmail.com>
2022-11-27 14:24:41 -0300

    fix

--- app/views/inc/verificarDocente.php
+++ app/views/inc/verificarDocente.php
@@ -8,5 +8,5 @@ if($_SESSION['type']==1){
 	header('Location: ResponsableHome.php');
 	exit();	
 }else{
-	header('Location: ../../index.php');}
-	exit();
+	header('Location: ../../index.php');exit();}
+	
