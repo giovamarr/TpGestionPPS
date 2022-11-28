39a914e4e5785241120c50ec6bd46c86a2e4fe3c
Giovanni Martin <giovamartin@gmail.com>
2022-11-28 09:06:32 -0300

    arreglo de validaciones

--- system/core/Conexion.php
+++ system/core/Conexion.php
@@ -4,15 +4,15 @@ class Conexion{
     
 
     public static function Conectar(){
-        // $dbhost	= "containers-us-west-97.railway.app:7959";
-        // $dbuser	= "root";		  // database username
-        // $dbpass	= "IMEc8SeZle1Xfqxx2wip";		     // database password
-        // $dbname	= "railway";    // database name
-
-        $dbhost	= "localhost:8111";	   // localhost or IP
+        $dbhost	= "containers-us-west-97.railway.app:7959";
         $dbuser	= "root";		  // database username
-        $dbpass	= "";		     // database password
-        $dbname	= "pps";    // database name
+        $dbpass	= "IMEc8SeZle1Xfqxx2wip";		     // database password
+        $dbname	= "railway";    // database name
+
+        // $dbhost	= "localhost:8111";	   // localhost or IP
+        // $dbuser	= "root";		  // database username
+        // $dbpass	= "";		     // database password
+        // $dbname	= "pps";    // database name
     try{
         return mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
     }catch (Exception $e){
