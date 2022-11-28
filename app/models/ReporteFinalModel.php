c08d3bd3d3db5f7e954c76d8516011a250529999
Agustin Etcheverry <etxeagus@gmail.com>
2022-11-28 00:35:40 -0300

    cambios consulta

--- app/models/ReporteFinalModel.php
+++ app/models/ReporteFinalModel.php
@@ -10,90 +10,111 @@ class ReporteFinalModel{
     }
 
     public function insertarReporte($conclusiones,$idPPS){
-        $ob = new Conexion();
-        $con=$ob->conectar();
-        $date=date('Y-m-d');
-        $query = "INSERT INTO finalreport (conclusiones, idPPS_FP,date ) VALUES ('$conclusiones',$idPPS,'$date')";
-        $result = mysqli_query($con, $query);
-        return $result;
-
+        if (is_numeric($conclusiones) and is_numeric($idPPS)){
+            $ob = new Conexion();
+            $con=$ob->conectar();
+            $date=date('Y-m-d');
+            $query = "INSERT INTO finalreport (conclusiones, idPPS_FP,date ) VALUES ('$conclusiones',$idPPS,'$date')";
+            $result = mysqli_query($con, $query);
+            return $result;
+        }
     }
 
     public function chequearsiexitePPS($idPPS){
-        $ob = new Conexion();
-        $con=$ob->conectar();        
-        $checksiexitePPS = "SELECT * FROM solicitudes WHERE idPPS = '$idPPS' and id_Profe is not null";
-        $vResultado = mysqli_query($con, $checksiexitePPS);
-        return $countsiexitePPS = mysqli_num_rows($vResultado);
+        if (is_numeric($idPPS)){
+            $ob = new Conexion();
+            $con=$ob->conectar();        
+            $checksiexitePPS = "SELECT * FROM solicitudes WHERE idPPS = '$idPPS' and id_Profe is not null";
+            $vResultado = mysqli_query($con, $checksiexitePPS);
+            return $countsiexitePPS = mysqli_num_rows($vResultado);
+        }
     }
 
     public function chequearsiYaenvioFR($idPPS){
-        $ob = new Conexion();
-        $con=$ob->conectar();        
-        $checksiYaenvioFR = "SELECT * FROM finalreport WHERE idPPS_FP = '$idPPS' ";				
-        $vResultado = mysqli_query($con, $checksiYaenvioFR);
-        return $countsiYaenvioFR = mysqli_num_rows($vResultado);
+        if (is_numeric($idPPS)){
+            $ob = new Conexion();
+            $con=$ob->conectar();        
+            $checksiYaenvioFR = "SELECT * FROM finalreport WHERE idPPS_FP = '$idPPS' ";				
+            $vResultado = mysqli_query($con, $checksiYaenvioFR);
+            return $countsiYaenvioFR = mysqli_num_rows($vResultado);
+        }
     }
     public function chequearsiTieneSeguisinaprobar($idPPS){
-        $ob = new Conexion();
-        $con=$ob->conectar();        
-        $TieneSeguisinaprobar = "SELECT * FROM seguimientos WHERE id_PPS = '$idPPS' and aprobadoSeg is null ";				
-        $vResultado = mysqli_query($con, $TieneSeguisinaprobar);
-        return $countsiTieneSeguisinaprobar = mysqli_num_rows($vResultado);
+        if (is_numeric($idPPS)){
+            $ob = new Conexion();
+            $con=$ob->conectar();        
+            $TieneSeguisinaprobar = "SELECT * FROM seguimientos WHERE id_PPS = '$idPPS' and aprobadoSeg is null ";				
+            $vResultado = mysqli_query($con, $TieneSeguisinaprobar);
+            return $countsiTieneSeguisinaprobar = mysqli_num_rows($vResultado);
+        }
     }
 
     public function getReportesFinales($idP){
-        $ob = new Conexion();
-        $con=$ob->conectar();
-        $vSql = "SELECT * FROM finalreport where idPPS_FP='$idP'";
-        $vResultado = mysqli_query($con, $vSql);
-        return $vResultado;  
+        if (is_numeric($idP)){
+            $ob = new Conexion();
+            $con=$ob->conectar();
+            $vSql = "SELECT * FROM finalreport where idPPS_FP='$idP'";
+            $vResultado = mysqli_query($con, $vSql);
+            return $vResultado;  
+        }
     }
 
     public function getReportesFinalesDocente($idProfe){
-        $ob = new Conexion();
-        $con=$ob->conectar();
-        $vSql = "SELECT * FROM finalreport f inner join solicitudes s on f.idPPS_FP=s.idPPS 
-                inner join users u on u.id=s.id_user where s.id_Profe='$idProfe' and (f.aprobadaFR is null)";
-        $vResultado = mysqli_query($con, $vSql);
-        return $vResultado;  
+        if (is_numeric($idProfe)){
+            $ob = new Conexion();
+            $con=$ob->conectar();
+            $vSql = "SELECT * FROM finalreport f inner join solicitudes s on f.idPPS_FP=s.idPPS 
+                    inner join users u on u.id=s.id_user where s.id_Profe='$idProfe' and (f.aprobadaFR is null)";
+            $vResultado = mysqli_query($con, $vSql);
+            return $vResultado;  
+        }
     }
     public function getReporteFinal($idPPS,$idFR){
-        $ob = new Conexion();
-        $con=$ob->conectar();
-        $vSql = "SELECT * FROM finalreport where idPPS_FP='$idPPS' and idFR='$idFR'";
-        $vResultado = mysqli_query($con, $vSql);
-        return $vResultado;  
+        if (is_numeric($idPPS) and is_numeric($idFR)){
+            $ob = new Conexion();
+            $con=$ob->conectar();
+            $vSql = "SELECT * FROM finalreport where idPPS_FP='$idPPS' and idFR='$idFR'";
+            $vResultado = mysqli_query($con, $vSql);
+            return $vResultado;  
+        }
     }
 
     public function getReporteFinalAdmin($idPPS){
-        $ob = new Conexion();
-        $con=$ob->conectar();
-        $vSql = "SELECT * FROM finalreport where idPPS_FP='$idPPS'";
-        $vResultado = mysqli_query($con, $vSql);
-        return $vResultado;  
+        if (is_numeric($idPPS)){
+            $ob = new Conexion();
+            $con=$ob->conectar();
+            $vSql = "SELECT * FROM finalreport where idPPS_FP='$idPPS'";
+            $vResultado = mysqli_query($con, $vSql);
+            return $vResultado;  
+        }
     }
 
     public function aprobarRF($idPPS,$idFR){
-        $ob = new Conexion();
-        $con=$ob->conectar();
-        $query = "UPDATE finalreport SET aprobadaFR=1 where idPPS_FP='$idPPS' and idFR='$idFR'";
-        $result = mysqli_query($con, $query);
-        return $result;
+        if (is_numeric($idPPS) and is_numeric($idFR)){
+            $ob = new Conexion();
+            $con=$ob->conectar();
+            $query = "UPDATE finalreport SET aprobadaFR=1 where idPPS_FP='$idPPS' and idFR='$idFR'";
+            $result = mysqli_query($con, $query);
+            return $result;
+        }
     }
     public function desaprobarRF($idPPS,$idFR){
-        $ob = new Conexion();
-        $con=$ob->conectar();
-        $query = "UPDATE finalreport SET aprobadaFR=2 , comentario='$comentario' where idPPS_FP='$idPPS' and idFR='$idFR'";
-        $result = mysqli_query($con, $query);
-        return $result;
+        if (is_numeric($idPPS) and is_numeric($idFR)){
+            $ob = new Conexion();
+            $con=$ob->conectar();
+            $query = "UPDATE finalreport SET aprobadaFR=2 , comentario='$comentario' where idPPS_FP='$idPPS' and idFR='$idFR'";
+            $result = mysqli_query($con, $query);
+            return $result;
+        }
     }
     public function getMailUserporRF($idP){
-        $ob = new Conexion();
-        $con=$ob->conectar();
-        $vSql = "SELECT u.email FROM  solicitudes s inner join users u on u.id=s.id_user where s.idPPS='$idP'";
-        $rf = mysqli_query($con, $vSql);
-        $vResultado=mysqli_fetch_array($rf);
-        return $vResultado['email'];
+        if (is_numeric($idP)){
+            $ob = new Conexion();
+            $con=$ob->conectar();
+            $vSql = "SELECT u.email FROM  solicitudes s inner join users u on u.id=s.id_user where s.idPPS='$idP'";
+            $rf = mysqli_query($con, $vSql);
+            $vResultado=mysqli_fetch_array($rf);
+            return $vResultado['email'];
+        }   
     }
 }
--- app/models/SeguimientosModel.php
+++ app/models/SeguimientosModel.php
@@ -19,78 +19,98 @@ class SeguimientosModel{
     }
 
     public function insertarSeguimiento($actividadesRealizadas, $actividadesProximas,$cuestionesPendientes,$experiencias, $desvioCronograma, $hsTrabajadas,$TotalhsTrabajadas,$idPPS){
-        $ob = new Conexion();
-        $con=$ob->conectar();
-        $query = "INSERT INTO seguimientos (actividadesRealizadas, actividadesProximas, cuestionesPendientes, experiencias, desvioCronograma, hsTrabajadas, TotalhsTrabajadas, id_PPS ) 
-                VALUES ('$actividadesRealizadas', '$actividadesProximas','$cuestionesPendientes','$experiencias', '$desvioCronograma', '$hsTrabajadas','$TotalhsTrabajadas',$idPPS)";
-        $result = mysqli_query($con, $query);
-        return $result;
+        if (
+            strlen($actividadesRealizadas) >= 5 and strlen($actividadesProximas) >= 5 and strlen($cuestionesPendientes) >= 5 and strlen($desvioCronograma) >= 5
+            and is_numeric($hsTrabajadas) and is_numeric($TotalhsTrabajadas) and is_numeric($idPPS)
+        ){
+            $ob = new Conexion();
+            $con=$ob->conectar();
+            $query = "INSERT INTO seguimientos (actividadesRealizadas, actividadesProximas, cuestionesPendientes, experiencias, desvioCronograma, hsTrabajadas, TotalhsTrabajadas, id_PPS ) 
+                    VALUES ('$actividadesRealizadas', '$actividadesProximas','$cuestionesPendientes','$experiencias', '$desvioCronograma', '$hsTrabajadas','$TotalhsTrabajadas',$idPPS)";
+            $result = mysqli_query($con, $query);
+            return $result;
+        }
     }
 
     public function chequearIDparaSegui($idPPS){
-        $ob = new Conexion();
-        $con=$ob->conectar();
-        $checkID = "SELECT * FROM solicitudes WHERE idPPS = '$idPPS' and id_Profe IS NOT NULL";
-	    $result = $con-> query($checkID);
-        return $count = mysqli_num_rows($result);
+        if (is_numeric($idPPS)){
+            $ob = new Conexion();
+            $con=$ob->conectar();
+            $checkID = "SELECT * FROM solicitudes WHERE idPPS = '$idPPS' and id_Profe IS NOT NULL";
+            $result = $con-> query($checkID);
+            return $count = mysqli_num_rows($result);
+        }
     }
     public function chequearsitieneIF($idPPS){
-        $ob = new Conexion();
-        $con=$ob->conectar();
-        $checkID = "SELECT * FROM finalreport WHERE idPPS_FP = '$idPPS'";
-	    $result = $con-> query($checkID);
-        return $count = mysqli_num_rows($result);
+        if (is_numeric($idPPS)){
+        
+            $ob = new Conexion();
+            $con=$ob->conectar();
+            $checkID = "SELECT * FROM finalreport WHERE idPPS_FP = '$idPPS'";
+            $result = $con-> query($checkID);
+            return $count = mysqli_num_rows($result);
+        }
     }
 
     public function getSegui($idP){
-        $ob = new Conexion();
-        $con=$ob->conectar();
-        $vSql = "SELECT * FROM seguimientos where id_PPS='$idP'";
-        $vResultado = mysqli_query($con, $vSql);
-        return $vResultado;
+        if (is_numeric($idP)){
+            $ob = new Conexion();
+            $con=$ob->conectar();
+            $vSql = "SELECT * FROM seguimientos where id_PPS='$idP'";
+            $vResultado = mysqli_query($con, $vSql);
+            return $vResultado;
+        }
     }
 
     public function getMailUserporSegui($idP){
-        $ob = new Conexion();
-        $con=$ob->conectar();
-        $vSql = "SELECT u.* FROM  solicitudes s inner join users u on u.id=s.id_user where s.idPPS='$idP'";
-        $segui = mysqli_query($con, $vSql);
-        $vResultado=mysqli_fetch_array($segui);
-        return $vResultado;
+        if (is_numeric($idP)){
+            $ob = new Conexion();
+            $con=$ob->conectar();
+            $vSql = "SELECT u.* FROM  solicitudes s inner join users u on u.id=s.id_user where s.idPPS='$idP'";
+            $segui = mysqli_query($con, $vSql);
+            $vResultado=mysqli_fetch_array($segui);
+            return $vResultado;
+        }
     }
     
 
     public function getSeguiDocente($idProfe){
-        $ob = new Conexion();
-        $con=$ob->conectar();
-        $vSql = "SELECT * FROM seguimientos se inner join solicitudes s on se.id_PPS=s.idPPS 
-                inner join users u on u.id=s.id_user where s.id_Profe='$idProfe' and (se.aprobadoSeg is null)";
-        $vResultado = mysqli_query($con, $vSql);
-        return $vResultado;
+        if (is_numeric($idProfe)){
+            $ob = new Conexion();
+            $con=$ob->conectar();
+            $vSql = "SELECT * FROM seguimientos se inner join solicitudes s on se.id_PPS=s.idPPS 
+                    inner join users u on u.id=s.id_user where s.id_Profe='$idProfe' and (se.aprobadoSeg is null)";
+            $vResultado = mysqli_query($con, $vSql);
+            return $vResultado;
+        }
     }
 
     public function getOne($idPPS,$idSeguimientos){
-        $ob = new Conexion();
-        $con=$ob->conectar();
-        $vSql = "SELECT * FROM seguimientos where id_PPS='$idPPS' and idSeguimientos='$idSeguimientos'";
-        $vResultado = mysqli_query($con, $vSql);
-        return $vResultado;
+        if (is_numeric($idPPS) and is_numeric($idSeguimientos)){
+            $ob = new Conexion();
+            $con=$ob->conectar();
+            $vSql = "SELECT * FROM seguimientos where id_PPS='$idPPS' and idSeguimientos='$idSeguimientos'";
+            $vResultado = mysqli_query($con, $vSql);
+            return $vResultado;
+        }
     }
     public function aprobarSegui($idPPS,$idSeguimientos){
-        $ob = new Conexion();
-        $con=$ob->conectar();
-        $query = "UPDATE seguimientos SET aprobadoSeg=1 where id_PPS='$idPPS' and idSeguimientos='$idSeguimientos'";
-        $result = mysqli_query($con, $query);
-        return $result;
+        if (is_numeric($idPPS) and is_numeric($idSeguimientos)){
+            $ob = new Conexion();
+            $con=$ob->conectar();
+            $query = "UPDATE seguimientos SET aprobadoSeg=1 where id_PPS='$idPPS' and idSeguimientos='$idSeguimientos'";
+            $result = mysqli_query($con, $query);
+            return $result;
+        }
     }
     public function desaprobarSegui($idPPS,$idSeguimientos,$comentario){
-        $ob = new Conexion();
-        $con=$ob->conectar();
-        $query = "UPDATE seguimientos SET aprobadoSeg=2 , comentario='$comentario' where id_PPS='$idPPS' and idSeguimientos='$idSeguimientos'";
-        $result = mysqli_query($con, $query);
-        return $result;
+        if (is_numeric($idPPS) and is_numeric($idSeguimientos) and strlen($actividadesRealizadas) >= 3){
+            $ob = new Conexion();
+            $con=$ob->conectar();
+            $query = "UPDATE seguimientos SET aprobadoSeg=2 , comentario='$comentario' where id_PPS='$idPPS' and idSeguimientos='$idSeguimientos'";
+            $result = mysqli_query($con, $query);
+            return $result;
+        }
     }
-
-
 
 }
